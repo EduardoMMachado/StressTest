00ebe5b4b210d548da8582e5cf402891b480f88e
Eduardo Maia Machado <e.maia.machado@gmail.com>
2018-05-10 22:09:01 -0300

    UPDATE: Data

--- stress.py
+++ stress.py
@@ -11,11 +11,11 @@ import sys
 import random
 import json
 
-ROUTE = 'http://35.196.226.41:8000/list'
-N_THREADS = 16
+ROUTE = 'http://35.196.226.41/list'
+N_THREADS = 64
 EXIT = False
 RANGE = 0.01
-MAX_RANGE = 100
+MAX_RANGE = 1000
 RESULT = {}
 
 class RequestLoopThread(threading.Thread):
@@ -39,8 +39,10 @@ class RequestLoopThread(threading.Thread):
 
             params = {
                 'range_start': round(RANGE, 2),
-                'range_end': round(RANGE + 0.01, 2)
+                'range_end': round(RANGE + 0.5, 2)
             }
+
+            RANGE += random.random()
 
             try:
                 if RESULT.get(int(time.time())):
@@ -63,14 +65,16 @@ class RequestLoopThread(threading.Thread):
                             'responses': 1
                         }
                     if r._content == {}:
+                        print('Empty data...')
                         EXIT = True
                 else:
+                    print('Error: status ' + str(r.status_code))
                     EXIT = True
 
             except:
+                print('Other fail')
                 EXIT = True
 
-            RANGE += random.random()
             time.sleep(self.delay)
 
 threads = []
@@ -78,6 +82,7 @@ for n in range(N_THREADS):
     t = RequestLoopThread(n, 'Thread-'+str(n), 0.1)
     t.start()
     threads.append(t)
+    time.sleep(0.1)
 
 for t in threads:
     t.join()
